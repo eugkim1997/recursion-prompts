37ad92cfe928d1a93dd8ce6a9218e5596ad60c01
Eugene <eugkim1997@gmail.com>
2020-06-20 13:55:49 -0400

    Completed problems 22, 23, 24

--- src/recursion.js
+++ src/recursion.js
@@ -297,7 +297,16 @@ var rMap = function(array, callback) {
 // var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
 // countKeysInObj(obj, 'r') // 1
 // countKeysInObj(obj, 'e') // 2
-var countKeysInObj = function(obj, key) {
+countKeysInObj = function(obj, key) {
+    var count = 0;
+    for (var i in obj) {
+        if (i === key) {
+            count++;
+        } else if (typeof obj[i] === 'object') {
+            count += countKeysInObj(obj[i], key);
+        }
+    }
+    return count;
 };
 
 // 23. Write a function that counts the number of times a value occurs in an object.
@@ -305,11 +314,29 @@ var countKeysInObj = function(obj, key) {
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
+    var count = 0;
+    for (var key in obj) {
+        if (obj[key] === value) {
+            count ++;
+        } else if (typeof obj[key] === 'object') {
+            count += countValuesInObj(obj[key], value);
+        }
+    }
+    return count;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
 var replaceKeysInObj = function(obj, oldKey, newKey) {
+    for (var i in obj) {
+        if (i === oldKey) {
+            obj[newKey] = obj[oldKey];
+            delete obj[oldKey];
+        } else if (typeof obj[i] === 'object') {
+            replaceKeysInObj(obj[i], oldKey, newKey);
+        }
+    }
+    return obj;
 };
 
 // 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
